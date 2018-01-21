e421fc1bd1abbabf71d6ec6bc855fb0856abd3a3
MashaKazachenok <kazachenokm@mail.ru>
2018-01-21 22:18:07 +0300

    preparation for project

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
-var app = new (require('express'))()
-var port = 3000
-//var dirname = 'D:/ReduxTest'
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-app.use(webpackHotMiddleware(compiler))
-app.get("/", function(req, res) {
-res.sendFile(__dirname + '/index.html')
-})
-app.listen(port, function(error) {
-if (error) {
-console.error(error)
-} else {
-console.info("==> Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-}
-})
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+const app = new (require('express'))();
+
+const port = 3000;
+// var dirname = 'D:/ReduxTest'
+const compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+app.use(webpackHotMiddleware(compiler));
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+app.listen(port, (error) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('==> Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,32 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
-devtool: 'cheap-module-eval-source-map',
-entry: [
-'webpack-hot-middleware/client',
-'./src/index'
-],
-output: {
-path: path.join(__dirname, 'dist'),
-filename: 'bundle.js',
-publicPath: '/static/'
-},
-plugins: [
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  plugins: [
 
-new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
 
-],
-module: {
-loaders: [
-{
-loaders: ['babel-loader'],
-include: [
-path.resolve(__dirname, "src"),
-],
-test: /\.js$/,
-//plugins: ['transform-runtime'],
-}
-]
-}
-}
+  ],
+  module: {
+    loaders: [
+      {
+        loaders: ['babel-loader'],
+        include: [
+          path.resolve(__dirname, 'src'),
+        ],
+        test: /\.js$/,
+        // plugins: ['transform-runtime'],
+      },
+    ],
+  },
+};
