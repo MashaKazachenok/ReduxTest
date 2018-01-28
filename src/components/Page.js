cc42d14e6d06a83c0f9cb4df47a588c93330f0e9
MashaKazachenok <kazachenokm@mail.ru>
2018-01-28 22:45:12 +0300

    added async action

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,24 +2,29 @@ import React, { PropTypes, Component } from 'react'
 
 export default class Page extends Component {
 onYearBtnClick(e) {
-this.props.setYear(+e.target.innerText)
+this.props.getPhotos(+e.target.innerText)
 }
 render() {
-const { year, photos } = this.props
-return <div className="ib page">
+const { year, photos, fetching } = this.props
+return <div className='ib page'>
 <p>
-<button className="btn" onClick={::this.onYearBtnClick}>2016</button>
-<button className="btn" onClick={::this.onYearBtnClick}>2015</button>
-<button className="btn" onClick={::this.onYearBtnClick}>2014</button>
+<button className='btn' onClick={::this.onYearBtnClick}>2016</button>{'        '}
+<button className='btn' onClick={::this.onYearBtnClick}>2015</button>{' '}
+<button className='btn' onClick={::this.onYearBtnClick}>2014</button>
 </p>
 <h3>{year} year</h3>
+{
+fetching ?
+<p>Loading...</p>
+: 
 <p>You have {photos.length} photo.</p>
-</div>
 }
-} 
+< /div>
+}
+}
 
 /* Page.propTypes = {
 year: PropTypes.number.isRequired,
 photos: PropTypes.array.isRequired,
-setYear: PropTypes.func.isRequired
+getPhotos: PropTypes.func.isRequired
 } */
